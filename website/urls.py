56a881870e3927e26778a90bcc552614f62aaa42
Ali Ghanbarian <94618081+agh78@users.noreply.github.com>
2025-10-15 11:37:42 +0330

    Template Folder for html files

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import About_view, Contact_view, Home_view
 urlpatterns = [
-    path('Home', Home_view),
+    path('', Home_view),
     path('About', About_view),
     path('Contact', Contact_view)
 ]
--- website/views.py
+++ website/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def Home_view(request):
-    return HttpResponse('<h1>Home page<h1>')  #this is html code
+    return render(request, 'index.html')
 
 def About_view(request):
-    return HttpResponse('<h1>About page<h1>')  #this is html code
+    return render(request, 'about.html')
 
 def Contact_view(request):
-    return HttpResponse('<h1>Contact page<h1>')  #this is html code
+    return render(request, 'contact.html')
+
 
 
